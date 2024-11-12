0b5448397f1658debdeb9fae89b7f6df4a2cd7cb
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2024-11-12 20:32:24 +0900

    IoContext를 관리하는 AsioIoContext 클래스 생성

---
+++ ServerCore/AsioIoContext.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "AsioIoContext.h"
+
+
+AsioIoContext::AsioIoContext()
+{
+}
+
+
+AsioIoContext::~AsioIoContext()
+{
+    if (m_ioContext)
+        delete m_ioContext;
+
+    if (m_work)
+        delete m_work;
+}
+
+bool AsioIoContext::Init()
+{
+    m_ioContext = new boost::asio::io_context();
+    m_work = new boost::asio::executor_work_guard<boost::asio::io_context::executor_type>(m_ioContext->get_executor());
+
+    return true;
+}
+
+bool AsioIoContext::Run()
+{
+    uint8 iCnt = std::thread::hardware_concurrency() / 2;
+
+    for (int i = 0; i < iCnt; ++i)
+    {
+        std::thread* tThread = new std::thread([this] {
+            m_ioContext->run();
+            });
+        m_asioThread.push_back(tThread);
+    }
+
+    return true;
+}
+
+bool AsioIoContext::Stop()
+{
+    delete m_ioContext;
+    m_ioContext = nullptr;
+
+    delete m_work;
+    m_work = nullptr;
+}
---
+++ ServerCore/AsioIoContext.h
@@ -0,0 +1,20 @@
+#pragma once
+class AsioIoContext
+{
+public:
+	AsioIoContext();
+	~AsioIoContext();
+
+public:
+	bool Init();
+	bool Run();
+	bool Stop();
+
+	boost::asio::io_context* GetIoContext() { return m_ioContext; }
+
+private:
+	boost::asio::io_context* m_ioContext;
+	boost::asio::executor_work_guard<boost::asio::io_context::executor_type>* m_work;
+	std::vector<std::thread*> m_asioThread;
+
+};
