f59408abb4f02ca17f47b8ca692e5204de0ff6a4
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2025-01-06 21:40:28 +0900

    PacketBuffer 개발

---
+++ ServerCore/PacketBuffer.cpp
@@ -0,0 +1,46 @@
+#include "pch.h"
+#include "PacketBuffer.h"
+
+void PacketBuffer::Write(const void* data, std::size_t size)
+{
+    if (WritableSize() < size)
+    {
+        throw std::overflow_error("Not enough space in buffer to write data.");
+    }
+
+    std::memcpy(&m_Buffer[m_WritePos], data, size);
+    m_WritePos += size;
+}
+
+void PacketBuffer::Read(void* outData, std::size_t size)
+{
+    if (ReadableSize() < size)
+    {
+        throw std::underflow_error("Not enough data in buffer to read.");
+    }
+
+    std::memcpy(outData, &m_Buffer[m_ReadPos], size);
+    m_ReadPos += size;
+}
+
+void PacketBuffer::Peek(void* outData, std::size_t size)
+{
+    if (ReadableSize() < size)
+    {
+        throw std::underflow_error("Not enough data in buffer to peek.");
+    }
+
+    std::memcpy(outData, &m_Buffer[m_ReadPos], size);
+}
+
+void PacketBuffer::DiscardReadData()
+{
+    if (m_ReadPos > 0)
+    {
+        std::size_t readable = ReadableSize();
+        std::memmove(&m_Buffer[0], &m_Buffer[m_ReadPos], readable);
+        m_ReadPos = 0;
+        m_WritePos = readable;
+    }
+}
+
---
+++ ServerCore/PacketBuffer.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+#include <cstring>
+#include <stdexcept>
+
+class PacketBuffer
+{
+public:
+    PacketBuffer(std::size_t bufferSize = 4096)
+        : m_Buffer(bufferSize), m_ReadPos(0), m_WritePos(0)
+    {
+    }
+
+    ~PacketBuffer() = default;
+
+    
+    void Write(const void* data, std::size_t size);
+    void Read(void* outData, std::size_t size);
+    void Peek(void* outData, std::size_t size);
+    void DiscardReadData();
+
+    std::size_t ReadableSize() const { return m_WritePos - m_ReadPos; }
+    std::size_t WritableSize() const { return m_Buffer.size() - m_WritePos; }
+
+    void Clear()
+    {
+        m_ReadPos = 0;
+        m_WritePos = 0;
+    }
+
+private:
+    std::vector<uint8_t> m_Buffer;
+    std::size_t m_ReadPos;
+    std::size_t m_WritePos;
+};
