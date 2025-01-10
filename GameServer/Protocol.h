c7d856030fcb5756f08edb0ded739f3a87d771e7
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2025-01-10 12:22:39 +0900

    Protocol 패킷 추가
    defEchoString 패킷 추가

--- GameServer/Protocol.h
+++ GameServer/Protocol.h
@@ -1,14 +1,27 @@
 #pragma once
-struct PacketHeader
+
+enum class PacketType : uint8
 {
-	short checkValue;
-	int PacketType;
-	int PacketSize;
+	defEchoString = 100,
 };
 
-struct Packet
-{
+// 21 bytes
+#pragma pack(push, 1)
+struct PacketHeader {
+	PacketType type;           // 기본 : 100
+	char checkSum[16];
+	uint32 size;
+};
+
+// 1 byte
+struct PacketTail {
+	uint8 value;
+};
+
+// 150 bytes
+struct Packet {
 	PacketHeader header;
-	char payload[128];
+	char		 payload[128];
+	PacketTail	 tail;              // 기본 : 255
 };
-
+#pragma pack (pop)
