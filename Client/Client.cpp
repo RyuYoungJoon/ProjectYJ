e43e0bd43fde6be26ed6e48cf1dac37b721c0d0e
RyuYoungJoon <74610708+RyuYoungJoon@users.noreply.github.com>
2025-01-11 21:52:34 +0900

    Client에서 ServerSession 생성

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -2,21 +2,58 @@
 #include "AsioService.h"
 #include "AsioSession.h"
 
+class ServerSession : public AsioSession
+{
+public:
+    ServerSession(boost::asio::io_context& iocontext, tcp::socket socket)
+        : AsioSession(iocontext, std::move(socket))
+    {
+
+    }
+
+    void OnSend(int32 len)
+    {
+        cout << "OnSend 호출" << endl;
+    }
+
+    int32 OnRecv(BYTE* buffer, int32 len)
+    {
+        return len;
+    }
+
+    void OnConnected()
+    {
+        cout << "[Info] Connected Server!" << endl;
+    }
+
+    void OnDisconnected()
+    {
+        cout << "[Info] Disconnected Server!" << endl;
+    }
+};
+
 int main()
 {
     try
     {
-        boost::asio::io_context ioContext;
+        boost::asio::io_context IoContext;
 
-        auto sessionMaker = [&ioContext]() -> std::shared_ptr<AsioSession>
+        auto sessionMaker = [&IoContext]() -> std::shared_ptr<AsioSession>
             {
-                return std::make_shared<AsioSession>(ioContext, tcp::socket(ioContext));
+                return std::make_shared<AsioSession>(IoContext, tcp::socket(IoContext));
             };
 
         std::string host = "127.0.0.1";
         short port = 27931;
 
-        auto clientService = std::make_shared<AsioClientService>(ioContext, host, port, sessionMaker);
+        auto clientService = std::make_shared<AsioClientService>(
+            IoContext,
+            host, 
+            port, 
+            [](boost::asio::io_context& iocontext, tcp::socket socket) -> std::shared_ptr<AsioSession> 
+            {
+                return std::make_shared<ServerSession>(iocontext, std::move(socket));
+            });
 
 
         if (clientService->Start())
